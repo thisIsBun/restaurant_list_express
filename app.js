c4384af22ef165a621267a0f6cd193d7dd422115
thisIsBun <bun.coding@gmail.com>
2025-05-05 17:49:11 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const engine = require('express-handlebars');
-const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routers = require('./routes')
-require('dotenv').config()
+require('./config/mongoose')
 
 const app = express();
 const port = 3000;
@@ -15,17 +14,6 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routers)
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected')
-})
 
 app.listen(port, () => {
   console.log(`Express server is listen now: http://localhost:${port}`);
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,17 +1,8 @@
-const mongoose = require('mongoose')
 const Restaurant = require('../restaurant')
 const data = require('./restaurants.json').results
-require('dotenv').config()
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error')
-})
+const db = require('../../config/mongoose')
 
 db.once('open', () => {
-  console.log('mongodb connected')
   for (let i = 0; i < data.length; i++) {
     Restaurant.create(data[i])
   }
