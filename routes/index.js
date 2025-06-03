fe623057c1715eee4860b24da6392fb394b014e8
thisIsBun <bun.coding@gmail.com>
2025-06-03 15:40:49 +0800

    feat: add user login router

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import home from './modules/home.js';
 import restaurants from './modules/restaurants.js';
+import users from './modules/users.js';
 
 const router = express.Router();
 
 router.use('/', home);
 router.use('/restaurants', restaurants);
+router.use('/users', users);
 
 export default router;
