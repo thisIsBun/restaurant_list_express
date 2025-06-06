be300e3dc0786e9a2f980bb682f3066f4622aae2
thisIsBun <bun.coding@gmail.com>
2025-06-06 15:32:38 +0800

    feat: add submit user register function

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import User from '../../models/user.js';
 const router = express.Router();
 
 router.get('/login', (req, res) => {
@@ -7,6 +8,24 @@ router.get('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
   res.render('register');
+});
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body;
+
+  User.findOne({ email })
+    .then((user) => {
+      if (user) {
+        console.log('user existed already');
+        res.render('register', { name, email, password, confirmPassword });
+      } else {
+        return User.create({ name, email, password });
+      }
+    })
+    .then(() => {
+      res.render('login');
+    })
+    .catch((error) => console.log(error));
 });
 
 export default router;
