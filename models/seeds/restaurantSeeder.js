82f3450f475c76aee6e7185f18a4f8f07b714176
thisIsBun <bun.coding@gmail.com>
2025-06-20 14:59:45 +0800

    feat: modify restaurant seed data

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,10 +1,32 @@
 import Restaurant from '../restaurant.js';
 import data from './restaurants.js';
 import db from '../../config/mongoose.js';
+import bcryptjs from 'bcryptjs';
+import User from '../user.js';
+
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678',
+};
 
 db.once('open', () => {
-  for (let i = 0; i < data.length; i++) {
-    Restaurant.create(data[i]);
-  }
-  console.log('seeder done');
+  const { name, email, password } = SEED_USER;
+
+  bcryptjs
+    .genSalt(10)
+    .then((salt) => bcryptjs.hashSync(password, salt))
+    .then((hash) => User.create({ name, email, password: hash }))
+    .then((user) => {
+      return Promise.all(
+        Array.from({ length: data.length }, (_, i) => {
+          const restaurant = { ...data[i], userId: user._id };
+          return Restaurant.create(restaurant);
+        }),
+      );
+    })
+    .then(() => {
+      console.log('seeder done');
+      process.exit();
+    });
 });
